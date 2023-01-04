1dabce3b796267e637774fc774ce338a179e65f5
Testarossa <austin.w.burrows@gmail.com>
2023-01-03 20:57:38 -0500

    Update main.lua

--- source/server/main.lua
+++ source/server/main.lua
@@ -1,4 +1,17 @@
-RegisterNetEvent("ND_CarSpawnerCommand:setVehicleOwned", function(props)
-    local src = source
-    exports["ND_VehicleSystem"]:setVehicleOwned(src, props, false)
+RegisterCommand('car', function(source, args)
+    local ped = GetPlayerPed(source)
+    local coords = GetEntityCoords(ped)
+    local veh = args[1]
+    local save = args[2]
+    local heading = GetEntityHeading(ped)
+    if veh == nil then veh = "adder" end
+    if save == nil then save = false end
+    local entity = CreateVehicleServerSetter(joaat(veh), 'automobile', coords + vector3( 0.0, 3.0, 0.5), heading)
+    if DoesEntityExist(entity) then
+        Wait(0)
+        local netID = NetworkGetNetworkIdFromEntity(entity)
+        TriggerClientEvent('saveCar',source, netID, entity, save)
+    else
+        print(("^1Failed to spawn vehicle '%s'^0"):format(v.model))
+    end
 end)
